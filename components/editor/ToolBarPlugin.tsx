0defd28874ddb50fa3e5b091b7f75b81860d1a7f
Wolf1610 <dutta2772004@gmail.com>
2025-06-05 20:20:55 +0530

    font family added

--- components/editor/ToolBarPlugin.tsx
+++ components/editor/ToolBarPlugin.tsx
@@ -12,6 +12,7 @@ export default function ToolbarPlugin() {
   const [editor] = useLexicalComposerContext();
   const [fontSize, setFontSize] = useState("16"); // default font size
   const [color, setColor] = useState("#000000");
+  const [fontFamily, setFontFamily] = useState("Times New Roman");
 
   const applyFontSize = useCallback(() => {
     editor.update(() => {
@@ -43,6 +44,15 @@ export default function ToolbarPlugin() {
       editor.dispatchCommand(FORMAT_TEXT_COMMAND, "italic");
     });
   }, [editor]);
+
+  const applyFontFamily = useCallback(() => {
+    editor.update(() => {
+      const selection = $getSelection();
+      if ($isRangeSelection(selection)) {
+        $patchStyleText(selection, { "font-family": fontFamily });
+      }
+    });
+  }, [editor, fontFamily]);
 
   return (
     <div className="flex gap-2 p-2">
@@ -81,6 +91,34 @@ export default function ToolbarPlugin() {
         onBlur={applyFontColor}
         className="w-8 h-8  border rounded bg-white"
       />
+
+      <select
+        value={fontFamily}
+        onChange={(e) => setFontFamily(e.target.value)}
+        onBlur={applyFontFamily}
+        className="border rounded p-1 bg-white text-black"
+      >
+        <option value="Times New Roman">Times New Roman</option>
+        <option value="Arial">Arial</option>
+        <option value="Calibri">Calibri</option>
+        <option value="Cambria">Cambria</option>
+        <option value="Georgia">Georgia</option>
+        <option value="Garamond">Garamond</option>
+        <option value="Trebuchet MS">Trebuchet MS</option>
+        <option value="Verdana">Verdana</option>
+        <option value="Helvetica">Helvetica</option>
+        <option value="Tahoma">Tahoma</option>
+        <option value="Segoe UI">Segoe UI</option>
+        <option value="Lucida Console">Lucida Console</option>
+        <option value="Courier New">Courier New</option>
+        <option value="Palatino Linotype">Palatino Linotype</option>
+        <option value="Arial Narrow">Arial Narrow</option>
+        <option value="Century Gothic">Century Gothic</option>
+        <option value="Book Antiqua">Book Antiqua</option>
+        <option value="Constantia">Constantia</option>
+        <option value="Franklin Gothic Medium">Franklin Gothic Medium</option>
+        <option value="Rockwell">Rockwell</option>
+      </select>
     </div>
   );
 }
